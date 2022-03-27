6bc90415b9ce209ea05566ddff0fa2b06757f515
fabriciored <fabriciojls@gmail.com>
2022-03-27 16:54:19 -0300

    feat: build header component

---
+++ app/components/modules/header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import logo from '../../../public/logo.png'
+import Button from '../elements/button';
+
+import styles from "./header.module.scss";
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.headerLeft}>
+            <Image className={styles.logo} src={logo} width='80px' height='80px'/>
+            </div>
+            <div className={styles.headerRight}>
+                <Button dark text='About'/>
+                <Button text='Contact'/>
+
+            </div>
+        </div>
+    )
+  };
+  
+  export default Header;
+  
