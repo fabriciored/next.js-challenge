0bf0e749f308c1475f48285d82c222ecfd32f694
fabriciored <fabriciojls@gmail.com>
2022-03-27 16:56:41 -0300

    feat: create main layout

---
+++ app/components/layouts/main.tsx
@@ -0,0 +1,12 @@
+import Header from "../modules/header";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+  };
+  
+  export default MainLayout;
+  
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
-import Button from '../app/components/elements/button'
+import Head from "next/head"
+import Header from '../app/components/modules/header'
+import MainLayout from '../app/components/layouts/main'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Head>
+      <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+      </Head>
+      <MainLayout/>
     </div>
   )
 }
